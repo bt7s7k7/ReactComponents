52e4965781b5d72d9ea95a36f6f3ae120adf3486
Branislav Trstenský <bt7s7k7@hotmail.sk>
2020-09-19 19:56:38 +0200

    Added Spacer component

--- src/sharedComponents/Grid/frameDeriv.tsx
+++ src/sharedComponents/Grid/frameDeriv.tsx
@@ -5,3 +5,4 @@ import { Frame, FrameProps } from "./Frame";
 
 export let Col = forwardRef<HTMLDivElement, FrameProps & BaseProps>(function Col(props, ref) { return <Frame {...{ ...props, direction: "column" }} ref={ref} /> })
 export let Row = forwardRef<HTMLDivElement, FrameProps & BaseProps>(function Row(props, ref) { return <Frame {...{ ...props, direction: "row" }} ref={ref} /> })
+export let Spacer = forwardRef<HTMLDivElement, FrameProps & BaseProps>(function Spacer(props, ref) { return <Frame {...{ ...props, fill: true }} ref={ref} /> })
